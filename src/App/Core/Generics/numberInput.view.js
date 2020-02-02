84fff2bdcb1c32c0388f614e3727b725e8d788e3
Adrian <adrianfdez469@gmail.com>
2020-02-01 22:08:05 -0500

    Agregando estilo al campo numerico

--- src/App/Core/Generics/numberInput.view.js
+++ src/App/Core/Generics/numberInput.view.js
@@ -37,12 +37,20 @@ const NumberInput = props => {
     const {value, handleChange, label} = props;
 
     return <TextField
+                style={{marginTop: '8px', marginBottom: '4px', width: '100%', border: 0,
+                  display: "inline-flex",
+                  padding: 0,
+                  position: "relative",
+                  minWidth: 0,
+                  flexDirection: "column",
+                  verticalAlign: "top"
+                }}
                 label={label}
                 value={value}
                 onChange={handleChange}
                 id="formatted-numberformat-input"
                 InputProps={{
-                inputComponent: NumberFormatCustom,
+                  inputComponent: NumberFormatCustom,
                 }}
             />;
 
